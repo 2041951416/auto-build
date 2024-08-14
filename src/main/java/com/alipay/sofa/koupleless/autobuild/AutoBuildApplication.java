88057023556a4ca0fb6f22d9783f761415eb0000
hxk <h911623516@gmail.com>
2024-08-14 23:45:02 +0800

    test

---
+++ src/main/java/com/alipay/sofa/koupleless/autobuild/AutoBuildApplication.java
@@ -0,0 +1,13 @@
+package com.alipay.sofa.koupleless.autobuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoBuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoBuildApplication.class, args);
+    }
+
+}
